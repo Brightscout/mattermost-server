1c427041ebb4113267ec880f40cd2f0599c550a9
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2019-01-21 14:46:18 +0530

    Merge branches MI-270 and MI-266

--- web/lti.go
+++ web/lti.go
@@ -1,9 +1,16 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
 package web
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/mattermost/mattermost-server/mlog"
 	"github.com/mattermost/mattermost-server/model"
@@ -11,10 +18,10 @@ import (
 )
 
 func (w *Web) InitLti() {
-	w.MainRouter.Handle("/login/lti", w.NewHandler(loginWithLti)).Methods("POST")
+	w.MainRouter.Handle("/login/lti", w.NewHandler(loginWithLTI)).Methods("POST")
 }
 
-func loginWithLti(c *Context, w http.ResponseWriter, r *http.Request) {
+func loginWithLTI(c *Context, w http.ResponseWriter, r *http.Request) {
 	mlog.Debug("Received an LTI Login request")
 
 	r.ParseForm()
@@ -25,41 +32,54 @@ func loginWithLti(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	mlog.Debug("Testing whether LTI is enabled: " + strconv.FormatBool(c.App.Config().LTISettings.Enable))
 	if !c.App.Config().LTISettings.Enable {
-		mlog.Error("LTI login request when LTI is disabled in config.json")
-		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
+		mlog.Error("Received LTI login request when LTI is disabled in config.json")
+		c.Err = model.NewAppError("loginWithLti", "api.lti.login.error.lti_disabled", nil, "", http.StatusNotImplemented)
 		return
 	}
 
-	// Validate request
 	mlog.Debug("Validating LTI request")
-	lmss := c.App.Config().LTISettings.GetKnownLMSs()
-	ltiConsumerKey := r.FormValue("oauth_consumer_key")
-	var ltiConsumerSecret string
-
-	for _, val := range lmss {
-		// TODO: Figure out a better way to find consumer secret for multiple LMSs
-		if lms, ok := val.(model.EdxLMSSettings); ok {
-			if lms.OAuth.ConsumerKey == ltiConsumerKey {
-				ltiConsumerSecret = lms.OAuth.ConsumerSecret
-				break
-			}
-		}
-	}
-
-	if ltiConsumerSecret == "" {
-		mlog.Error("Consumer secret not found for consumer key: " + ltiConsumerKey)
+	if !utils.ValidateLTIRequest(c.GetSiteURLHeader()+c.Path, c.App.Config().LTISettings.GetKnownLMSs(), r) {
 		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
 		return
 	}
 
-	p := utils.NewProvider(ltiConsumerSecret, c.GetSiteURLHeader()+c.Path)
-	p.ConsumerKey = ltiConsumerKey
-	if ok, err := p.IsValid(r); err != nil || ok == false {
-		mlog.Error("Invalid LTI request: " + err.Error())
-		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
-		return
-	}
+	mlog.Debug("Setting LTI Launch Data cookie")
+	setLTILaunchDataCookie(c, w, r)
 
 	mlog.Debug("Redirecting to the LTI signup page")
 	http.Redirect(w, r, c.GetSiteURLHeader()+"/signup_lti", http.StatusFound)
+}
+
+func encodeLTIRequest(v url.Values) string {
+	if v == nil {
+		return ""
+	}
+	form := make(map[string]string)
+	for key, value := range v {
+		form[key] = value[0]
+	}
+	res, err := json.Marshal(form)
+	if err != nil {
+		mlog.Error("Error in json.Marshal: " + err.Error())
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(string(res)))
+}
+
+func setLTILaunchDataCookie(c *Context, w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // to populate r.Form
+	maxAge := 600 // 10 minutes
+	expiresAt := time.Unix(model.GetMillis()/1000+int64(maxAge), 0)
+	cookie := &http.Cookie{
+		Name:     model.LTI_LAUNCH_DATA_COOKIE,
+		Value:    encodeLTIRequest(r.Form),
+		Path:     "/",
+		MaxAge:   maxAge,
+		Expires:  expiresAt,
+		Domain:   c.App.GetCookieDomain(),
+		HttpOnly: false,
+	}
+
+	http.SetCookie(w, cookie)
 }
